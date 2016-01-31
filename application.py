982fa26a9a06d3e6da10e45cf912136ccc2a5776
Sakarya <alpersakarya@gmail.com>
2016-01-31 15:07:51 -0500

    Improvements and stop option added

--- application.py
+++ application.py
@@ -1,21 +1,75 @@
-import boto, os
+import boto, os, time, sys
 from blessings import Terminal
 
 ec2 = boto.connect_ec2()
 instances = ec2.get_only_instances()
 t = Terminal()
 
-# Describe instances the way I want to see
-print t.blue("#########################################################")
 
-def color_i_state():
-	if instance.state == "running":
-		return t.green(instance.state)
-	elif instance.state == "stopped":
-		return t.red(instance.state)
-	else:
-		return t.yellow(instance.state)
+def main_function():
+    os.system('clear')
+    number = 1
+    n = len(instances)
 
-for instance in instances:
-	print "-- ", t.bold("Instance:") ,instance.id, "||", t.bold("State:"), color_i_state(), "||", t.bold("Public IP:"), instance.ip_address, "||", t.bold("Name:") ,instance.tags['Name'] 
-print t.blue("#########################################################")
+    # Coloring of running/stopped/other states
+    def color_i_state():
+        if instance.state == "running":
+            return t.green(instance.state)
+        elif instance.state == "stopped":
+            return t.red(instance.state)
+        else:
+            return t.yellow(instance.state)
+
+    # Describe instances the way I want to see
+    print t.blue("##########################################################################################")
+    print "TOTAL INSTANCES:", n
+    for instance in instances:
+        color_i_state()
+        # looking to see if instance tag exist or not
+        if 'Name' in instance.tags:
+            print number, t.bold("Instance:"), instance.id, "||", t.bold("State:"), color_i_state(), "||", t.bold("Public IP:"), instance.ip_address, "||", t.bold("Name:"), instance.tags['Name']
+            number = (number + 1)
+        else:
+            print number, t.bold("Instance:"), instance.id, "||", t.bold("State:"), color_i_state(), "||", t.bold("Public IP:"), instance.ip_address, "||", t.bold("Name:"), "no tag OR not available now"
+            number = (number + 1)
+
+    print t.blue("##########################################################################################")
+    print "Refresh instance list - Press 0"
+    print "Start an Instance     - Press 1"
+    print "Stop  an Instance     - Press 2"
+    print "Ctrl + C to exit or   - Press 9"
+
+    operation = raw_input()
+    if operation == "0":
+        main_function()
+
+    elif operation == "1":
+        selected_instance = int(raw_input("What is the instance number? e.g 1 or 2 "))
+        selected_instance = (selected_instance - 1)
+        instances[selected_instance].start()
+        print "Instance is starting..."
+        time.sleep(3)
+        main_function()
+
+    elif operation == "2":
+        selected_instance = int(raw_input("What is the instance number? e.g 1 or 2 "))
+        selected_instance = (selected_instance - 1)
+        instances[selected_instance].stop()
+        print "Instance is stopping..."
+        time.sleep(3)
+        main_function()
+
+    elif operation == "9":
+        print "GOOD BYE..."
+        exit()
+
+    else:
+        print t.red("Wrong selection")
+        time.sleep(3)
+        main_function()
+
+try:
+    main_function()
+except KeyboardInterrupt:
+    print "GOOD BYE..."
+    sys.exit(0)
